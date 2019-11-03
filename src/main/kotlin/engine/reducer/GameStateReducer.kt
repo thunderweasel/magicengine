9d69fcdc6ec69722dfee1aafca6a3369dafe8907
Ryan McLeod <excitinglyboring@gmail.com>
2019-11-03 01:08:04 -0400

    Make starting hand size a constant

--- src/main/kotlin/engine/reducer/GameStateReducer.kt
+++ src/main/kotlin/engine/reducer/GameStateReducer.kt
@@ -5,10 +5,12 @@ import engine.domain.drawCards
 import engine.model.GameStart
 import engine.model.GameState
 
+private const val STARTING_HAND_SIZE = 7
+
 fun reduceGameState(action: GameAction, state: GameState) =
     when(action) {
         is GameAction.ChooseFirstPlayer -> state.copy(
-            players = state.players.map { it.drawCards(7) },
+            players = state.players.map { it.drawCards(STARTING_HAND_SIZE) },
             gameStart = GameStart.Mulligans(action.chosenPlayer)
         )
     }

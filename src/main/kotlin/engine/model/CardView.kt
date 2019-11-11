9e486a5baa2dbda2c54cf0c4bb3999501f8f86a3
Ryan McLeod <excitinglyboring@gmail.com>
2019-11-10 23:52:58 -0500

    Tweak comments

--- src/main/kotlin/engine/model/CardView.kt
+++ src/main/kotlin/engine/model/CardView.kt
@@ -1,5 +1,8 @@
 package engine.model
 
+/**
+ * Representation of a card that may or may not be known to the player depending on the game state.
+ */
 sealed class CardView {
     data class KnownCard(val card: Card): CardView(), Card by card {
         override fun toString() = name
--- src/main/kotlin/engine/model/PlayerState.kt
+++ src/main/kotlin/engine/model/PlayerState.kt
@@ -3,8 +3,9 @@ package engine.model
 /**
  * Representation of a player's state in a game.
  *
- * The type of card can be swapped out here. For example [PlayerStateView] uses type [CardView] in order to
- * allow cards to be hidden in the [GameStateView].
+ * The type of card is just [Card] in [PlayerState], but [PlayerStateGeneric] can be used to have a state with a different
+ * type of card. This is used by [PlayerStateView] to create a version of player state where all cards are represented
+ * by a [CardView], which can be known or unknown depending on the game state.
  */
 data class PlayerStateGeneric<CARD_TYPE : Any>(
     val id: PlayerId,

f9fd30c161de318d1f0e8882dc0701c35d49ce86
Ryan McLeod <excitinglyboring@gmail.com>
2019-11-13 22:31:00 -0500

    Add kotlinter

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.41"
+    id("org.jmailen.kotlinter") version "2.1.2"
 }
 
 version = "1.0-SNAPSHOT"

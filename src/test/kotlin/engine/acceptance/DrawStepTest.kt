0d726e8d3166b9c8b82d0df2b96fc56b06ae6ef0
Ryan McLeod <excitinglyboring@gmail.com>
2019-12-01 13:10:54 -0500

    Cleaner assertions for checking the result of actions

--- src/test/kotlin/engine/acceptance/DrawStepTest.kt
+++ src/test/kotlin/engine/acceptance/DrawStepTest.kt
@@ -1,12 +1,8 @@
 package engine.acceptance
 
 import assertk.assertThat
-import engine.acceptance.DrawStepStates.drawStepTurn1
-import engine.acceptance.DrawStepStates.drawStepTurn2
-import engine.acceptance.DrawStepStates.upkeepAlicePriorityTurn2
-import engine.acceptance.DrawStepStates.upkeepBobPriorityTurn1
 import engine.action.PassPriority
-import engine.assertions.matchesState
+import engine.assertions.actionResultsInState
 import engine.factories.DeckFactory
 import engine.factories.PlayerStateFactory
 import engine.factories.PlayerStateFactory.ID_ALICE
@@ -17,7 +13,6 @@ import engine.state.DrawStep
 import engine.state.GameState
 import engine.state.Turn
 import engine.state.UpkeepStep
-import engine.state.noPendingRandomization
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
@@ -27,68 +22,64 @@ class DrawStepTest {
     @Test
     fun `on the first turn, active player does not draw a card`() {
         assertThat(
-            reducer(upkeepBobPriorityTurn1.noPendingRandomization(), PassPriority(ID_BOB))
-        ).matchesState(drawStepTurn1)
+            GameState(
+                players = listOf(
+                    PlayerStateFactory.createAlice(hand = DeckFactory.alice.slice(0..6)),
+                    PlayerStateFactory.createBob(hand = DeckFactory.bob.slice(0..6))
+                ),
+                temporalPosition = Turn(
+                    activePlayer = ID_ALICE,
+                    phase = BeginningPhase(step = UpkeepStep),
+                    priority = ID_BOB,
+                    firstTurn = true
+                )
+            )
+        ).actionResultsInState(
+            action = PassPriority(ID_BOB),
+            expectedState = GameState(
+                players = listOf(
+                    PlayerStateFactory.createAlice(hand = DeckFactory.alice.slice(0..6)),
+                    PlayerStateFactory.createBob(hand = DeckFactory.bob.slice(0..6))
+                ),
+                temporalPosition = Turn(
+                    activePlayer = ID_ALICE,
+                    phase = BeginningPhase(step = DrawStep),
+                    priority = ID_ALICE,
+                    firstTurn = true
+                )
+            )
+        )
     }
 
     @Test
     fun `when it's not the first turn, active player draws a card`() {
         assertThat(
-            reducer(upkeepAlicePriorityTurn2.noPendingRandomization(), PassPriority(ID_ALICE))
-        ).matchesState(drawStepTurn2)
+            GameState(
+                players = listOf(
+                    PlayerStateFactory.createAlice(hand = DeckFactory.alice.slice(0..6)),
+                    PlayerStateFactory.createBob(hand = DeckFactory.bob.slice(0..6))
+                ),
+                temporalPosition = Turn(
+                    activePlayer = ID_BOB,
+                    phase = BeginningPhase(step = UpkeepStep),
+                    priority = ID_ALICE,
+                    firstTurn = false
+                )
+            )
+        ).actionResultsInState(
+            action = PassPriority(ID_ALICE),
+            expectedState = GameState(
+                players = listOf(
+                    PlayerStateFactory.createAlice(hand = DeckFactory.alice.slice(0..6)),
+                    PlayerStateFactory.createBob(hand = DeckFactory.bob.slice(0..7)) // draw a card
+                ),
+                temporalPosition = Turn(
+                    activePlayer = ID_BOB,
+                    phase = BeginningPhase(step = DrawStep),
+                    priority = ID_BOB,
+                    firstTurn = false
+                )
+            )
+        )
     }
-}
-
-private object DrawStepStates {
-    val upkeepBobPriorityTurn1 = GameState(
-        players = listOf(
-            PlayerStateFactory.createAlice(hand = DeckFactory.alice.slice(0..6)),
-            PlayerStateFactory.createBob(hand = DeckFactory.bob.slice(0..6))
-        ),
-        temporalPosition = Turn(
-            activePlayer = ID_ALICE,
-            phase = BeginningPhase(step = UpkeepStep),
-            priority = ID_BOB,
-            firstTurn = true
-        )
-    )
-
-    val drawStepTurn1 = GameState(
-        players = listOf(
-            PlayerStateFactory.createAlice(hand = DeckFactory.alice.slice(0..6)),
-            PlayerStateFactory.createBob(hand = DeckFactory.bob.slice(0..6))
-        ),
-        temporalPosition = Turn(
-            activePlayer = ID_ALICE,
-            phase = BeginningPhase(step = DrawStep),
-            priority = ID_ALICE,
-            firstTurn = true
-        )
-    )
-
-    val upkeepAlicePriorityTurn2 = GameState(
-        players = listOf(
-            PlayerStateFactory.createAlice(hand = DeckFactory.alice.slice(0..6)),
-            PlayerStateFactory.createBob(hand = DeckFactory.bob.slice(0..6))
-        ),
-        temporalPosition = Turn(
-            activePlayer = ID_BOB,
-            phase = BeginningPhase(step = UpkeepStep),
-            priority = ID_ALICE,
-            firstTurn = false
-        )
-    )
-
-    val drawStepTurn2 = GameState(
-        players = listOf(
-            PlayerStateFactory.createAlice(hand = DeckFactory.alice.slice(0..6)),
-            PlayerStateFactory.createBob(hand = DeckFactory.bob.slice(0..7)) // draw a card
-        ),
-        temporalPosition = Turn(
-            activePlayer = ID_BOB,
-            phase = BeginningPhase(step = DrawStep),
-            priority = ID_BOB,
-            firstTurn = false
-        )
-    )
 }
--- src/test/kotlin/engine/assertions/GameStateAssertions.kt
+++ src/test/kotlin/engine/assertions/GameStateAssertions.kt
@@ -5,7 +5,31 @@ import assertk.all
 import assertk.assertions.isEqualTo
 import assertk.assertions.isNull
 import assertk.assertions.prop
+import engine.action.GameAction
+import engine.reducer.StatePendingRandomizationReducer
+import engine.reducer.masterReducer
+import engine.state.GameState
 import engine.state.StatePendingRandomization
+import engine.state.noPendingRandomization
+
+fun Assert<GameState>.actionResultsInState(
+    action: GameAction,
+    expectedState: GameState
+) = transform { actual ->
+    actual.noPendingRandomization()
+}.actionResultsInState(
+    reducer = masterReducer(),
+    action = action,
+    expectedState = expectedState
+)
+
+fun <STATE_TYPE> Assert<StatePendingRandomization<STATE_TYPE>>.actionResultsInState(
+    reducer: StatePendingRandomizationReducer<STATE_TYPE>,
+    action: GameAction,
+    expectedState: STATE_TYPE
+) = transform { actual ->
+    reducer(actual, action)
+}.matchesState(expectedState)
 
 fun <STATE_TYPE> Assert<StatePendingRandomization<STATE_TYPE>>.matchesState(expected: STATE_TYPE) =
     all {
